fb1a90f3e21e8c7b900f72f0b0be2d56d403ded7
Marcos <rorshach200@gmail.com>
2022-05-24 16:47:53 +0200

    Animaciones principales y variables

---
+++ script.js
@@ -0,0 +1,10 @@
+// VARIABLES
+let comida1 = document.getElementById("comida1");
+let comida2 = document.getElementById("comida2");
+let comida3 = document.getElementById("comida3");
+let comida4 = document.getElementById("comida4");
+let comida5 = document.getElementById("comida5");
+let comida6 = document.getElementById("comida6");
+let comida7 = document.getElementById("comida7");
+let comida8 = document.getElementById("comida8");
+let comida9 = document.getElementById("comida9");
